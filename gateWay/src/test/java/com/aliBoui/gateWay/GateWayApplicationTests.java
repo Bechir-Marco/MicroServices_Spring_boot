9426d9e85c7723bd3443e7d2ebf5442e6a013368
Bechir-Marco <bechir.marko@gmail.com>
2023-10-27 12:38:05 +0100

    project structure

---
+++ gateWay/src/test/java/com/aliBoui/gateWay/GateWayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aliBoui.gateWay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GateWayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
